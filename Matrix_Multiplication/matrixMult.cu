124c3b7bd53c97140e8c44636b50ba92d2654218
Charbel Chucri <charbel11chucri@gmail.com>
2021-08-08 01:30:02 +0300

    Refactored matrix multiplication code

--- Matrix_Multiplication/matrixMult.cu
+++ Matrix_Multiplication/matrixMult.cu
@@ -1,15 +1,7 @@
-#include "common.h"
+#include "matrixMult.h"
 #include "../Helper_Code/timer.h"
 
 #define BLOCK_DIM 32
-
-#define cudaErrorCheck(error) { gpuAssert((error), __FILE__, __LINE__); }
-void gpuAssert(cudaError_t code, const char *file, const int line) {
-    if (code != cudaSuccess) {
-		fprintf(stderr, "CUDA Error: %s in file %s at line %d\n", cudaGetErrorString(code), file, line);
-		exit(code);
-	}
-}
 
 // A (MxK) * B (KxN) = C (MxN)
 __global__ void matrixMultiplicationKernel(float* A, float* B, float* C, unsigned int M, unsigned int N, unsigned int K) {
@@ -62,57 +54,42 @@ void matrixMultiplicationGPU(float* A, float* B, float* C, unsigned int M, unsig
 
     //Allocating GPU memory
     startTime(&timer);
-
     float *A_d, *B_d, *C_d;
-    cudaError_t errMallocA = cudaMalloc((void **) &A_d, M * K * sizeof(float)); cudaErrorCheck(errMallocA);
-    cudaError_t errMallocB = cudaMalloc((void **) &B_d, K * N * sizeof(float)); cudaErrorCheck(errMallocB);
-    cudaError_t errMallocC = cudaMalloc((void **) &C_d, M * N * sizeof(float)); cudaErrorCheck(errMallocC);
-
+    cudaMalloc((void **) &A_d, M * K * sizeof(float));
+    cudaMalloc((void **) &B_d, K * N * sizeof(float));
+    cudaMalloc((void **) &C_d, M * N * sizeof(float));
     cudaDeviceSynchronize();
     stopTime(&timer);
     printElapsedTime(timer, "GPU Allocation time");
 
     //Copying data to GPU from Host
     startTime(&timer);
-
-    cudaError_t errMemcpyA = cudaMemcpy(A_d, A, M * K * sizeof(float), cudaMemcpyHostToDevice); cudaErrorCheck(errMemcpyA);
-    cudaError_t errMemcpyB = cudaMemcpy(B_d, B, K * N * sizeof(float), cudaMemcpyHostToDevice); cudaErrorCheck(errMemcpyB);
-
+    cudaMemcpy(A_d, A, M * K * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B, K * N * sizeof(float), cudaMemcpyHostToDevice);
     cudaDeviceSynchronize();
     stopTime(&timer);
     printElapsedTime(timer, "Copying to GPU time");
 
     //Calling kernel
     startTime(&timer);
-
     dim3 numThreadsPerBlock(BLOCK_DIM, BLOCK_DIM);
     dim3 numBlocks((N + numThreadsPerBlock.x - 1) / numThreadsPerBlock.x, (M + numThreadsPerBlock.y - 1) / numThreadsPerBlock.y);
     if (type == 1) { matrixMultiplicationKernel<<< numBlocks, numThreadsPerBlock >>>(A_d, B_d, C_d, M, N, K); }
 	else { matrixMultiplicationKernelWithTiling<<< numBlocks, numThreadsPerBlock >>>(A_d, B_d, C_d, M, N, K); }
-    cudaErrorCheck(cudaGetLastError());			//For arguments errors
-    cudaErrorCheck(cudaDeviceSynchronize());	//For execution error in the kernel
-
     stopTime(&timer);
     printElapsedTime(timer, "Running the kernel time", GREEN);
 
     //Copying data from GPU to Host
     startTime(&timer);
-
-    cudaError_t errMemcpyC = cudaMemcpy(C, C_d, M * N * sizeof(float), cudaMemcpyDeviceToHost); cudaErrorCheck(errMemcpyC);
-
+    cudaMemcpy(C, C_d, M * N * sizeof(float), cudaMemcpyDeviceToHost);
     cudaDeviceSynchronize();
     stopTime(&timer);
     printElapsedTime(timer, "Copying from GPU time");
 
     //Freeing GPU memory
     startTime(&timer);
-
-    cudaError_t errFreeA = cudaFree(A_d); cudaErrorCheck(errFreeA);
-    cudaError_t errFreeB = cudaFree(B_d); cudaErrorCheck(errFreeB);
-    cudaError_t errFreeC = cudaFree(C_d); cudaErrorCheck(errFreeC);
-
+    cudaFree(A_d); cudaFree(B_d); cudaFree(C_d); 
     cudaDeviceSynchronize();
     stopTime(&timer);
     printElapsedTime(timer, "GPU Deallocation time");
 }
-
--- Matrix_Multiplication/matrixMult.h
+++ Matrix_Multiplication/matrixMult.h
@@ -1,5 +1,5 @@
-#ifndef _COMMON_H_
-#define _COMMON_H_
+#ifndef _MATRIXMULT_H_
+#define _MATRIXMULT_H_
 
 void matrixMultiplicationGPU(float* A, float* B, float* C, unsigned int M, unsigned int N, unsigned int K, unsigned int type);
 
